3cef625a3d212c7bb1fbe757de09d08a111b4623
rokmrhar <rokmrhar9@gmail.com>
2026-01-13 10:14:37 +0100

    na kmetiji je lepo push

---
+++ DrugaVaja/DrugaVaja/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DrugaVaja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rad imam C#!");
+        }
+    }
+}
---
+++ VajaZaBonus/VajaZaBonus/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace VajaZaBonus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Na kmetiji je lepo");
+            Console.WriteLine("I-A-I-A-O");
+            Console.WriteLine("Tam imamo osla dva");
+            Console.WriteLine("I-A-I-A-O");
+        }
+    }
+}
